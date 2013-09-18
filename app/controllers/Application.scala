fd0b03d40594373d92a6cb0b39e90a7d2b3a2e54
Alan Raison <alanraison@users.sourceforge.net>
2013-09-18 22:03:16 +0100

    Simple form and test page for ref updates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,24 @@ package controllers
 
 import play.api.mvc._
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import play.api.data._
+import play.api.data.Forms._
+import models.{RefUpdate, RepoUpdate}
 
 object Application extends Controller {
 
   val repos = new Repositories
+
+  val updateForm = Form(
+    mapping(
+      "repoName" -> text,
+      "updates" -> seq(mapping(
+        "branch" -> text,
+        "fromRef" -> text,
+        "toRef" -> text
+      )(RefUpdate.apply)(RefUpdate.unapply))
+    )(RepoUpdate.apply)(RepoUpdate.unapply)
+  )
 
   def index = Action {
     Ok(views.html.index(
@@ -17,6 +31,17 @@ object Application extends Controller {
     Ok(views.html.summary(repo)(
       FileRepositoryBuilder.create(repos.pathFor(repo))
     ))
+  }
+
+  def add = Action {
+    Ok(views.html.add(updateForm))
+  }
+
+  def refUpdate = Action { implicit req =>
+    updateForm.bindFromRequest.fold(
+      formWithErrors => BadRequest,
+      value => Ok
+    )
   }
 
 }
